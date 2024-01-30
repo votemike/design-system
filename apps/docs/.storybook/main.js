4c3d0b642533ddd3f8b39ca7cc42552c490b07be
Michael Gwynne <michael.gwynne@meandem.com>
2024-01-30 13:54:58 +0000

    Add storybook testing

--- apps/docs/.storybook/main.js
+++ apps/docs/.storybook/main.js
@@ -10,6 +10,7 @@ const config = {
     getAbsolutePath("@storybook/addon-links"),
     getAbsolutePath("@storybook/addon-essentials"),
     getAbsolutePath("@storybook/addon-docs"),
+    getAbsolutePath("@storybook/addon-interactions"),
   ],
   framework: {
     name: getAbsolutePath("@storybook/react-vite"),
